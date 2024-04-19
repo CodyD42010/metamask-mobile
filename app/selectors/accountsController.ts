5661482422d0bf6428c51e874266b3c65135f835
Owen Craston <owengc12@gmail.com>
2024-04-19 13:04:34 -0700

    these screens are broken

--- app/selectors/accountsController.ts
+++ app/selectors/accountsController.ts
@@ -1,6 +1,7 @@
 import { createSelector } from 'reselect';
 import { AccountsControllerState } from '@metamask/accounts-controller';
 import { RootState } from '../reducers';
+import Engine from '../core/Engine';
 import { toChecksumAddress } from 'ethereumjs-util';
 
 const selectAccountsControllerState = (state: RootState) =>
@@ -17,6 +18,18 @@ export const selectSelectedInternalAccount = createSelector(
 export const selectSelectedInternalAccountAddressAsChecksum = createSelector(
   selectAccountsControllerState,
   (accountsControllerState: AccountsControllerState) => {
+    console.log(
+      'accounts/ engine preferences',
+      JSON.stringify(Engine.context.PreferencesController.state, null, 2),
+    );
+    console.log(
+      'accounts/ engine',
+      JSON.stringify(Engine.context.AccountsController, null, 2),
+    );
+    console.log(
+      'accounts/',
+      JSON.stringify(selectAccountsControllerState, null, 2),
+    );
     const accountId = accountsControllerState.internalAccounts.selectedAccount;
     const selectedAddress =
       accountsControllerState.internalAccounts.accounts[accountId].address;
